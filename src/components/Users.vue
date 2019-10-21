65085bc6e16b678b473fdbf60066891bddfe748b
Rifatul I <rifatul.istiaque@gmail.com>
2019-10-20 20:50:49 -0400

    Refactored delete UI.

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,29 +1,32 @@
 <template>
-<div class="row">
-    <div class="col-12">
-        <h1>List Users</h1>
-    </div>
-    <div class="col-12">
-        <ul class="list-group">
-
-            <li
-                v-for="(user,index) in users"
-                :key="index"
-                @click="deleteUser(index)"
-                class="list-group-item"
+    <div class="row">
+        <div class="col-12">
+            <h1>List Users</h1>
+        </div>
+        <div class="col-12">
+            <ul class="list-group">
+                <li
+                    v-for="(user,index) in users"
+                    :key="index"
+                    class="list-group-item d-flex justify-content-between"
+                >
+                    <span>
+                        {{user.name}} ( {{user.email}} )
+                    </span>
+                    <span class="deleteUser" @click="deleteUser(index)">
+                        X
+                    </span>
+                </li>
+            </ul>
+        </div>
+        <div class="col-12 mt-5">
+            <button 
+                @click="openAddUser" 
+                class="btn btn-primary"
             >
-            {{user.name}} ( {{user.email}} )</li>
-
-        </ul>
+            Add User</button>
+        </div>
     </div>
-    <div class="col-12 mt-5">
-        <button 
-            @click="openAddUser" 
-            class="btn btn-primary"
-        >
-        Add User</button>
-    </div>
-</div>
 </template>
 <script>
 export default {
@@ -50,8 +53,10 @@ export default {
     },
     methods:{
         deleteUser(index){
-            this.users.splice(index,1)
-            alert("Succes Deleted!")
+            if (confirm(`Do you want to delete ${this.users[index]['name']}?`)){
+                this.users.splice(index,1)
+                alert("Succes Deleted!")
+            }
         },
         openAddUser(){
             this.$emit('addOpened', true)
@@ -62,11 +67,18 @@ export default {
 <style lang="scss">
     ul{
         li{
-            cursor: pointer
+            cursor: pointer;
+            margin-bottom: 1px!important;
         }
         li:hover{
-            background-color: red;
-            color: white
+            border: 1px solid red;
         }
+    }
+    .deleteUser{
+        font-size: 20px;
+        color: red;
+    }
+    .deleteUser:hover{
+        font-weight: 700!important;
     }
 </style>
